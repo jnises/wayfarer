25faf750d6dd28bb594b46109a4403ceba767533
joel nises <joel.nises@gmail.com>
2021-04-05 15:34:00 +0200

    audio cleanup

--- src/audio.rs
+++ src/audio.rs
@@ -1,9 +1,10 @@
 use crate::message::Message;
 use crate::midi::NoteEvent;
 use crate::synth::Synth;
+use anyhow::{anyhow, Result};
 use cpal::{
     traits::{DeviceTrait, HostTrait, StreamTrait},
-    OutputCallbackInfo, SampleFormat, SampleRate,
+    OutputCallbackInfo, SampleFormat,
 };
 use crossbeam::channel;
 use std::thread::{self, JoinHandle};
@@ -19,30 +20,31 @@ impl AudioManager {
         // TODO callback trait instead of channel
         message_sender: channel::Sender<Message>,
     ) -> Self {
-        let (tx, rx) = channel::bounded(1);
+        let (shutdown_tx, shutdown_rx) = channel::bounded(1);
         // run this in a thread since it causes errors if run before the gui on a thread
         let handle = thread::spawn(move || {
-            let host = cpal::default_host();
-            let device = host
-                .default_output_device()
-                .expect("no default output device found");
-            let supported_config = device
-                .supported_output_configs()
-                .unwrap()
-                .filter(|config| {
-                    config.sample_format() == SampleFormat::F32 && config.channels() == 2
-                })
-                .next()
-                .unwrap();
-            let min_rate = supported_config.min_sample_rate();
-            let max_rate = supported_config.max_sample_rate();
-            let config = supported_config
-                .with_sample_rate(SampleRate(48000).clamp(min_rate, max_rate))
-                .config();
-            let mut synth = Synth::new(config.sample_rate.0, 2);
-            let message_sender_clone = message_sender.clone();
-            let stream = device
-                .build_output_stream(
+            // emulate try block
+            match (|| -> Result<_> {
+                let host = cpal::default_host();
+                let device = host
+                    .default_output_device()
+                    .ok_or_else(|| anyhow!("no output audio device found"))?;
+                let supported_config = device
+                    .supported_output_configs()?
+                    .filter(|config| {
+                        // only stereo configs
+                        config.sample_format() == SampleFormat::F32 && config.channels() == 2
+                    })
+                    // just pick the first valid config
+                    .next()
+                    .ok_or_else(|| anyhow!("no valid output audio config found"))?;
+                let default_sample_rate = device.default_output_config()?.sample_rate();
+                let config = supported_config
+                    .with_sample_rate(default_sample_rate)
+                    .config();
+                let mut synth = Synth::new(config.sample_rate.0, 2);
+                let message_sender_clone = message_sender.clone();
+                let stream = device.build_output_stream(
                     &config,
                     move |data: &mut [f32], _: &OutputCallbackInfo| {
                         while let Ok(event) = midi_events.try_recv() {
@@ -63,17 +65,28 @@ impl AudioManager {
                             .send(Message::Status(format!("error: {:?}", error)))
                             .unwrap();
                     },
-                )
-                .unwrap();
-            message_sender
-                .send(Message::AudioName(device.name().unwrap()))
-                .unwrap();
-            stream.play().unwrap();
-            rx.recv().unwrap();
+                )?;
+                message_sender
+                    .send(Message::AudioName(device.name().unwrap()))
+                    .unwrap();
+                // can't return stream since it isn't Send
+                stream.play()?;
+                // return the stream to keep it alive
+                Ok(stream)
+            })() {
+                Ok(_stream) => {
+                    shutdown_rx.recv().unwrap();
+                }
+                Err(e) => {
+                    message_sender
+                        .send(Message::Status(format!("error: {}", e)))
+                        .unwrap();
+                }
+            }
         });
         Self {
             handle: Some(handle),
-            shutdown: tx,
+            shutdown: shutdown_tx,
         }
     }
 }
