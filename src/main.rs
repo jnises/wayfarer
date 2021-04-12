82b505462c1305478038c54e3e71f0bd5cb187e1
Joel Nises <joel.nises@gmail.com>
2021-04-12 14:07:34 +0200

    added onscreen keyboard

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 #![warn(clippy::all, rust_2018_idioms)]
+use std::{collections::HashSet, convert::TryFrom};
+
 use crossbeam::channel;
-use eframe::{egui::{self, Vec2}, epi::{self, App}};
+use eframe::{
+    egui::{self, Ui, Vec2},
+    epi::{self, App},
+};
 
 mod midi;
 use midi::MidiReader;
@@ -8,8 +13,17 @@ mod audio;
 use audio::{AudioManager, Message};
 mod synth;
 use synth::Synth;
+use wmidi::MidiMessage;
 
 const NAME: &'static str = "Wayfärer";
+
+type MidiSender = channel::Sender<MidiMessage<'static>>;
+
+fn is_key_black(note: wmidi::Note) -> bool {
+    [
+        false, true, false, true, false, false, true, false, true, false, true, false,
+    ][(u8::from(note) % 12) as usize]
+}
 
 type MessageReceiver = channel::Receiver<Message>;
 struct Wayfarer {
@@ -17,12 +31,15 @@ struct Wayfarer {
     midi_interface_name: String,
     audio_interface_name: String,
     status_text: String,
+    midi_tx: MidiSender,
+    keyboard_pressed: HashSet<egui::Id>,
 }
 
 struct WayfarerArgs {
     audio_messages: MessageReceiver,
     midi_name: String,
     initial_status: String,
+    midi_tx: MidiSender,
 }
 
 impl Wayfarer {
@@ -32,7 +49,48 @@ impl Wayfarer {
             midi_interface_name: args.midi_name,
             audio_interface_name: "-".to_string(),
             status_text: args.initial_status,
+            midi_tx: args.midi_tx,
+            keyboard_pressed: HashSet::new(),
         }
+    }
+
+    // need to keep state in self since egui doesn't seem to have support for custom state currently
+    fn on_screen_keyboard(&mut self, ui: &mut Ui) {
+        ui.horizontal(|ui| {
+            for note_num in 60.. {
+                if ui.available_width() <= 0f32 {
+                    break;
+                }
+                let note = match wmidi::Note::try_from(note_num) {
+                    Ok(note) => note,
+                    Err(_) => break,
+                };
+                let b = egui::Button::new(" ").fill(Some(if is_key_black(note) {
+                    egui::Color32::BLACK
+                } else {
+                    egui::Color32::WHITE
+                }));
+                let r = ui.add(b);
+                // egui doesn't seem to have any convenient "pressed" or "released" event
+                if r.is_pointer_button_down_on() {
+                    if !self.keyboard_pressed.insert(r.id) {
+                        let _ = self.midi_tx.try_send(MidiMessage::NoteOn(
+                            wmidi::Channel::Ch1,
+                            note,
+                            wmidi::Velocity::from_u8_lossy(127),
+                        ));
+                    }
+                } else {
+                    if self.keyboard_pressed.remove(&r.id) {
+                        let _ = self.midi_tx.try_send(MidiMessage::NoteOff(
+                            wmidi::Channel::Ch1,
+                            note,
+                            wmidi::Velocity::from_u8_lossy(0),
+                        ));
+                    }
+                }
+            }
+        });
     }
 }
 
@@ -49,7 +107,8 @@ impl App for Wayfarer {
     }
 
     fn is_resizable(&self) -> bool {
-        false
+        //false
+        true
     }
 
     fn update(&mut self, ctx: &egui::CtxRef, _frame: &mut epi::Frame<'_>) {
@@ -76,6 +135,7 @@ impl App for Wayfarer {
                 ui.label(&self.audio_interface_name);
             });
             ui.label(&self.status_text);
+            self.on_screen_keyboard(ui);
         });
     }
 }
@@ -84,7 +144,7 @@ fn main() {
     let (audio_messages_tx, audio_messages_rx) = channel::bounded(256);
     let (miditx, midirx) = channel::bounded(256);
     // keeping _midi around so that we keep receiving midi events
-    let (_midi, midi_name, initial_status) = match MidiReader::new(miditx) {
+    let (_midi, midi_name, initial_status) = match MidiReader::new(miditx.clone()) {
         Ok(midi) => {
             let name = midi.get_name().to_string();
             (Some(midi), name, String::new())
@@ -101,6 +161,7 @@ fn main() {
         audio_messages: audio_messages_rx,
         midi_name,
         initial_status,
+        midi_tx: miditx,
     }));
     eframe::run_native(app);
 }
